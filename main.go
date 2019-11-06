0bb0a9a6fbe997df62bcf5cce3bddabd955718a4
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2019-11-06 22:25:10 +0800

    Add standard calls

--- main.go
+++ main.go
@@ -7,5 +7,26 @@ import (
 )
 
 func main() {
-	fmt.Println("Private Key:", keygen.GenPrivateKey())
+	privateKey := keygen.GenPrivateKey()
+	fmt.Println("Private Key:", privateKey)
+	fmt.Println()
+
+	wif, _ := keygen.Private2WIF(privateKey, false, true)
+	fmt.Println("WIF:", wif)
+
+	publicKey, _ := keygen.Private2Public(privateKey, false)
+	fmt.Println("Public Key:", publicKey)
+
+	address, _ := keygen.Public2Address(publicKey, true)
+	fmt.Println("Address:", address)
+	fmt.Println()
+
+	wif, _ = keygen.Private2WIF(privateKey, true, true)
+	fmt.Println("WIF (Compressed):", wif)
+
+	publicKey, _ = keygen.Private2Public(privateKey, true)
+	fmt.Println("Public Key (Compressed):", publicKey)
+
+	address, _ = keygen.Public2Address(publicKey, true)
+	fmt.Println("Address (Compressed):", address)
 }
