ae5fc125923ff331c6ee57d094599ab008feef6e
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2019-11-06 22:03:11 +0800

    Add Private2Public function

--- keygen/assoc.go
+++ keygen/assoc.go
@@ -4,13 +4,101 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
+	"math/big"
+	"reflect"
 
 	"golang.org/x/crypto/ripemd160"
 )
 
+type point struct {
+	x *big.Int
+	y *big.Int
+}
+
+var p *big.Int
+
+func init() {
+	p, _ = new(big.Int).SetString("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F", 16)
+}
+
+func ellipticAdd(q point, n point) point {
+	var lam *big.Int
+	if reflect.DeepEqual(q, n) {
+		lam = new(big.Int).Mul(
+			new(big.Int).Mul(
+				big.NewInt(3),
+				new(big.Int).Mul(n.x, n.x),
+			),
+			new(big.Int).Exp(
+				new(big.Int).Mul(big.NewInt(2), n.y),
+				new(big.Int).Sub(p, big.NewInt(2)),
+				p,
+			))
+	} else {
+		lam = new(big.Int).Mul(
+			new(big.Int).Sub(q.y, n.y),
+			new(big.Int).Exp(
+				new(big.Int).Sub(q.x, n.x),
+				new(big.Int).Sub(p, big.NewInt(2)),
+				p,
+			))
+	}
+
+	q.x = new(big.Int).Mod(
+		new(big.Int).Sub(
+			new(big.Int).Mul(lam, lam),
+			new(big.Int).Add(q.x, n.x),
+		),
+		p,
+	)
+
+	q.y = new(big.Int).Mod(
+		new(big.Int).Sub(
+			new(big.Int).Mul(lam, new(big.Int).Sub(n.x, q.x)),
+			n.y,
+		),
+		p,
+	)
+
+	return q
+}
+
 // Private2Public returns the public key associated with a private key (hex string)
-func Private2Public(privateKey string, compressed bool) string {
-	return ""
+func Private2Public(privateKey string, compressed bool) (string, error) {
+	if !IsPrivateKeyValid(privateKey) {
+		return "", fmt.Errorf("%s is not a valid key", privateKey)
+	}
+
+	// base point
+	var g, q point
+	g.x, _ = new(big.Int).SetString("79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798", 16)
+	g.y, _ = new(big.Int).SetString("483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8", 16)
+
+	// compute g * privateKey with repeated addition
+	x, _ := new(big.Int).SetString(privateKey, 16)
+	pow2, big2 := big.NewInt(1), big.NewInt(2)
+	for i := 0; i < 256; i++ {
+		if big.NewInt(0).Cmp(new(big.Int).And(x, pow2)) == -1 {
+			if reflect.DeepEqual(q, point{}) {
+				q = g
+			} else {
+				q = ellipticAdd(q, g)
+			}
+		}
+
+		g = ellipticAdd(g, g)
+		pow2.Mul(pow2, big2)
+	}
+
+	if compressed {
+		if big.NewInt(0).Cmp(new(big.Int).And(q.y, big.NewInt(1))) == -1 {
+			return "03" + q.x.Text(16), nil
+		}
+
+		return "02" + q.x.Text(16), nil
+	}
+
+	return "04" + q.x.Text(16) + q.y.Text(16), nil
 }
 
 // Public2Address returns the address associated with a public key
@@ -32,12 +120,17 @@ func Public2Address(publicKey string, mainnet bool) (string, error) {
 }
 
 // Private2Address returns the address associated with a private key
-func Private2Address(privateKey string, compressed bool, mainnet bool) (string, error) {
+func Private2Address(privateKey string, compressed, mainnet bool) (string, error) {
 	if !IsPrivateKeyValid(privateKey) {
 		return "", fmt.Errorf("%s is not a valid key", privateKey)
 	}
 
-	address, err := Public2Address(Private2Public(privateKey, compressed), mainnet)
+	publicKey, err := Private2Public(privateKey, compressed)
+	if err != nil {
+		return "", err
+	}
+
+	address, err := Public2Address(publicKey, mainnet)
 	if err != nil {
 		return "", err
 	}
@@ -46,7 +139,7 @@ func Private2Address(privateKey string, compressed bool, mainnet bool) (string, 
 }
 
 // Private2WIF returns the Wallet Import Format (WIF) associated with a private key (hex string)
-func Private2WIF(privateKey string, compressed bool, mainnet bool) (string, error) {
+func Private2WIF(privateKey string, compressed, mainnet bool) (string, error) {
 	if !IsPrivateKeyValid(privateKey) {
 		return "", fmt.Errorf("%s is not a valid key", privateKey)
 	}
