be6ac2ec9b95c23b1e4c406960c8ce241a4c0481
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2019-11-06 22:06:42 +0800

    Remove const usage

--- keygen/private_key.go
+++ keygen/private_key.go
@@ -5,19 +5,21 @@ import (
 	"math/big"
 )
 
-// N = (1 << 256) - 0x14551231950B75FC4402DA1732FC9BEBF
-const N = "fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141"
+// order
+var n *big.Int
+
+func init() {
+	n, _ = new(big.Int).SetString("fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141", 16)
+}
 
 // GenPrivateKey generates a hex encoded private key
 func GenPrivateKey() string {
-	n, _ := new(big.Int).SetString(N, 16)
 	privateKey, _ := rand.Int(rand.Reader, n)
 	return privateKey.Text(16)
 }
 
 // IsPrivateKeyValid checks if a given string is a valid private key
 func IsPrivateKeyValid(privateKey string) bool {
-	n, _ := new(big.Int).SetString(N, 16)
 	privateKeyInt, ok := new(big.Int).SetString(privateKey, 16)
 	if !ok {
 		return false
