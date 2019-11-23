962905400535397457591ac5e1b5de7367d94b49
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2019-11-24 00:14:40 +0800

    More comment improvement

--- keygen/base58_check.go
+++ keygen/base58_check.go
@@ -19,22 +19,22 @@ func init() {
 
 // Encode a version byte and payload to creates a Base58Check string.
 func Encode(version, payload string) (base58Str string, err error) {
-	// concatenate the version and payload
+	// Concatenate the version and payload.
 	extKey := version + payload
 	decoded, err := hex.DecodeString(extKey)
 	if err != nil {
 		return "", err
 	}
 
-	// take the checksum of the extended key
+	// Take the checksum of the extended key.
 	sum := sha256.Sum256(decoded)
 	sum = sha256.Sum256(sum[:])
 	checksum := sum[:4]
 
-	// concatenate the external key and checksum
+	// Concatenate the external key and checksum.
 	extKey += hex.EncodeToString(checksum)
 
-	// convert the external key to base-58
+	// Convert the external key to base-58.
 	var outStr string
 	x, _ := new(big.Int).SetString(extKey, 16)
 	for x.Cmp(big.NewInt(0)) == 1 {
@@ -43,19 +43,19 @@ func Encode(version, payload string) (base58Str string, err error) {
 		outStr = string(codeStr[rem.Int64()]) + outStr
 	}
 
-	// represent leading zero bytes by "1"
+	// Represent leading zero bytes by "1".
 	leadingOnes := strings.Repeat("1", (len(extKey)-len(strings.TrimLeft(extKey, "0")))/2)
 
-	// concatenate the 1's with the external key in base-58
+	// Concatenate the 1's with the external key in base-58.
 	return leadingOnes + outStr, nil
 }
 
 // Decode a Base58Check string to a version byte and payload.
 func Decode(base58Str string) (version, payload string) {
-	// strip leading 1's
+	// Strip the leading 1's.
 	outStr := strings.TrimLeft(base58Str, "1")
 
-	// construct byte string
+	// Construct the byte string.
 	x, pow58 := big.NewInt(0), big.NewInt(1)
 	for i := range outStr {
 		x.Add(x, new(big.Int).Mul(pow58, invCodeStr[outStr[len(outStr)-1-i]]))
@@ -66,7 +66,7 @@ func Decode(base58Str string) (version, payload string) {
 	leadingZeroes := strings.Repeat("00", len(base58Str)-len(outStr)) + strings.Repeat("0", len(byteStr)&1)
 	byteStr = leadingZeroes + byteStr
 
-	// drop checksum
+	// Drop the checksum.
 	return byteStr[:2], byteStr[2 : len(byteStr)-8]
 }
 
